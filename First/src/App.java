944c236ce91a76289d700c5271f00d88a259f9a2
erman.ozardic <erman.ozardic@ERMANOZARDIC.defence.local>
2019-03-10 21:22:41 +0300

    revert from remote

--- First/src/App.java
+++ First/src/App.java
@@ -4,7 +4,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("firsst");
-System.out.println("second for local2");
+System.out.println("second for local");
 	}
 
 }
