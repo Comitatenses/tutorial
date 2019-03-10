a446e9ada0ba341baf78dd6fb1b178fc4c0498c7
erman.ozardic <erman.ozardic@ERMANOZARDIC.defence.local>
2019-03-10 21:28:50 +0300

    asdsad

--- First/src/App.java
+++ First/src/App.java
@@ -3,8 +3,8 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("firsst");
-System.out.println("second for local234");
+		System.out.println("firsst");
+		System.out.println("second for local234");
 	}
 
 }
