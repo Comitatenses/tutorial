393ec2635ee484febe776812c4ab095a7ac004fc
erman.ozardic <erman.ozardic@ERMANOZARDIC.defence.local>
2019-03-22 09:39:57 +0300

    wiretap example

--- camel-activemq/src/main/java/com/javainuse/SimpleRouteBuilder.java
+++ camel-activemq/src/main/java/com/javainuse/SimpleRouteBuilder.java
@@ -1,16 +1,27 @@
 package com.javainuse;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
 public class SimpleRouteBuilder extends RouteBuilder {
-	//configure route for jms component
-    @Override
-    public void configure() throws Exception {
-        from("file:C:/inputFolder").split().tokenize("\n").to("jms:queue:javainuse");
-        /* splitter EIP pattern is implemented above to split the file. defined jms queue is called javainuse at activemq console 
-         * jms 		 --> protocol
-         * queue:javainuse 	 --> context path(a.k.a topic)
-         * in this example javainuse topic will be created */
+	@Override
+    public void configure() throws Exception {  //in this example 2 line was entered to file at inputfolder
+		// this will create 2 messages for deadletterqueue and wiretapend queues
+        from("file:C:/inputFolder").split().tokenize("\n").to("direct:test1");
+        
+        from("direct:test1")
+        //Wire Tap:Suppose get some error so send seperate copies of the message to 
+        //DeadLetter queue and also to direct:test2 
+        .wireTap("jms:queue:DeadLetterQueue")
+        .to("direct:test2");
+        
+        from("direct:test2")
+        .process(new Processor() {
+            public void process(Exchange arg0) throws Exception {
+              //Some logic here
+            	System.out.println(arg0.getIn().getBody().toString());
+            }
+      }).to("jms:queue:wiretapend");
     }
-
 }
